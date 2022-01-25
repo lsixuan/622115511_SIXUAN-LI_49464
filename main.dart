dcd8dc2e436f69fea16b56fa9159ba5168f06596
lsixuan <94843156+lsixuan@users.noreply.github.com>
2022-01-25 16:26:47 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,39 +1,110 @@
-import 'package:flutter/material.dart';
-import 'MyHomePage.dart';
-import 'MySetting.dart';
 
-void main() => runApp(MyApp());
-class MyApp extends StatefulWidget {
-  @override
-  _ScaffoldRouteState createState() => _ScaffoldRouteState();
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    
+    print(response.body);
+    print(jsonDecode(response.body));
+    print(Album.fromJson(jsonDecode(response.body)));
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
 
-class _ScaffoldRouteState extends State<MyApp> {
-  int _selectedIndex = 0;
-  List <Widget>list = [
-    MyHomePage(),
-    MySetting(),
-  ];
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: /*list[_selectedIndex]*/MyHomePage(),
-        // bottomNavigationBar: BottomNavigationBar(
-        //   items:  [
-        //     BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('Home')),
-        //     BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text('MySetting')),
-        //   ],
-        //   currentIndex: _selectedIndex,
-        //   onTap: _onItemTapped,
-        // ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Test API'),
     );
   }
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+late Future<Album> futureAlbum;
+
+@override
+void initState(){
+  super.initState();
+  futureAlbum =fetchAlbum();
+}
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+       title: Text(widget.title),
+       
+      ),
+      body: Center(
+        child:FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+          }
+        
+          ) 
+          ,),
+    );
+  }
+}
+
+class Album{
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json){
+    return Album(
+    userId: json['userId'], 
+    id: json['id'],
+    title: json['title'],
+     );
   }
 }
